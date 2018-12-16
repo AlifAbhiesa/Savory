9a6cf399615bf7dc3e1a540f5192ac66bbdd693a
M. Alif Abhiesa Al Kautsar <abhiesa24@gmail.com>
2018-12-16 21:32:49 +0700

    edit form list menu

--- app/src/main/java/com/thesavorycake/thesavory/GridItemActivity.java
+++ app/src/main/java/com/thesavorycake/thesavory/GridItemActivity.java
@@ -1,15 +1,27 @@
 package com.thesavorycake.thesavory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GridItemActivity extends AppCompatActivity {
 
-
+    TextView name;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_item);
+
+        name = findViewById(R.id.griddata);
+        image = findViewById(R.id.imageView);
+
+        Intent intent = getIntent();
+        name.setText(intent.getStringExtra("name"));
+        image.setImageResource(intent.getIntExtra("image"));
     }
 }

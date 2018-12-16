c2eaadd3758810733a3a08b0ebd156074739e3ca
M. Alif Abhiesa Al Kautsar <abhiesa24@gmail.com>
2018-12-16 19:27:37 +0700

    form list menu

---
+++ app/src/main/java/com/thesavorycake/thesavory/GridItemActivity.java
@@ -0,0 +1,15 @@
+package com.thesavorycake.thesavory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GridItemActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_item);
+    }
+}
